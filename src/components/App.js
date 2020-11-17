ba15c1dfe8b2568f6223106c7ee098d788d626b6
Darshan <dhulswar591@gmail.com>
2020-11-18 01:12:55 +0530

    App.js file modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './App.css'
 
 export class App extends Component {
     render() {
         return (
-            <div>
+            <div className="App">
                 Hello World
             </div>
         )
